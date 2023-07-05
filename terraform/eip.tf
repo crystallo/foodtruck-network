1255999ac7496c5360bda5cfd9e88ebf7f0e2f9b
crystallo <crystallo0v0@gmail.com>
2023-07-04 18:14:41 -0700

    fixed bracket

--- terraform/eip.tf
+++ terraform/eip.tf
@@ -1,11 +1,11 @@
 resource "aws_eip" "eip_1" {
-    depends_on = {
+    depends_on = [
         aws_internet_gateway.igw
-    }
+    ]
 }
 
 resource "aws_eip" "eip_2" {
-    depends_on = {
+    depends_on = [
         aws_internet_gateway.igw
-    }
+    ]
 }
